66a31f35cc80f25add4c3dc930a43c3d7b9c34ee
Zanpacto <alighanem982@gmail.com>
2023-03-16 13:43:29 +0200

    Update script.js

--- cars website11/js/script.js
+++ cars website11/js/script.js
@@ -6,14 +6,6 @@ menu.onclick = () =>{
   navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick = () =>{
-  document.querySelector('.login-form-container').classList.toggle('active');
-}
-
-document.querySelector('#close-login-form').onclick = () =>{
-  document.querySelector('.login-form-container').classList.remove('active');
-}
-
 window.onscroll = () =>{
 
   menu.classList.remove('fa-times');
@@ -24,5 +16,31 @@ window.onscroll = () =>{
   }else{
     document.querySelector('.header').classList.remove('active');
   };
+
+};
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    let speed = elm.getAttribute('data-speed');
+
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
+  });
+
+};
+
+
+document.querySelector('.home').onmouseleave = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    elm.style.transform = `translateX(0px) translateY(0px)`;
+
+  });
 
 };
